a0bc2da88af1c9d0c353232f76ae05c0856ba269
Даня <danpozh@yandex.ru>
2023-04-06 16:56:25 +0300

    zaliv

---
+++ js/xb_game.js
@@ -0,0 +1,49 @@
+$('document').ready(function(){
+    loadGoods();
+});
+
+function loadGoods() {
+    //загружаю товары на страницу
+    $.getJSON('../json/game_xb.json', function (data) {
+        console.log(data);
+        var out = '';
+        
+        
+            out+='<tr>'
+
+            let i=0
+            
+            for (var key in data){
+                out+='<td>'
+                
+                out+='<div class="product-inner">'
+                out+='<div class="product-wrap">'
+                out+='<img src="'+data[key].image+'" width="307px" height="500px">'
+                out+='<div class="actions">'
+                out+='<a href="" class="add-to-cart"></a>'
+                
+                out+='<a href="" class="wishlist"></a>'
+                out+='</div>'
+                out+='</div>'
+                out+='<div class="product-info">'
+                out+='<h3 class="product-title"><a href="">'+data[key]['name']+'</a></h3>'
+                out+='<span class="price">₽'+data[key]['cost']+'</span>'
+                out+='</div>'
+                out+='<td>'
+                
+                if(i==2){
+                    i=0
+                    out+='<tr>'
+                }else{
+                    i++
+                }
+            }
+            
+            out+='<tr>'
+        
+        
+        
+        
+        $('#goods').html(out);
+    })
+}
